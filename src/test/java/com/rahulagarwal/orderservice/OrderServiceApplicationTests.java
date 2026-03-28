e97c474ce66baff1411131914a62924672100e62
Rahul Agarwal <iamrpg07@gmail.com>
2026-03-28 22:12:55 +0530

    Initial Setup

---
+++ src/test/java/com/rahulagarwal/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahulagarwal.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
