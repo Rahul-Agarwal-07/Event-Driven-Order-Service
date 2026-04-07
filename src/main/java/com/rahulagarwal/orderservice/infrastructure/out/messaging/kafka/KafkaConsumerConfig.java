3baf6024f9f7cb815043e25a351d01990329ec0b
Rahul Agarwal <iamrpg07@gmail.com>
2026-04-07 23:33:32 +0530

    Added Kafka Retries and DLQ

---
+++ src/main/java/com/rahulagarwal/orderservice/infrastructure/out/messaging/kafka/KafkaConsumerConfig.java
@@ -0,0 +1,33 @@
+package com.rahulagarwal.orderservice.infrastructure.out.messaging.kafka;
+
+import org.apache.kafka.common.TopicPartition;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Bean
+    public DeadLetterPublishingRecoverer recoverer(KafkaTemplate<String, Object> kafkaTemplate) {
+        return new DeadLetterPublishingRecoverer(
+                kafkaTemplate,
+                (record, ex) -> {
+                    System.out.println("Sending to DLQ: " + record.value());
+                    return new TopicPartition("order-events-dlq", record.partition());
+                }
+        );
+    }
+
+    @Bean
+    public DefaultErrorHandler errorHandler(DeadLetterPublishingRecoverer recoverer) {
+
+        FixedBackOff backOff = new FixedBackOff(2000L, 3); // 3 retries, 2 sec gap
+
+        return new DefaultErrorHandler(recoverer, backOff);
+    }
+
+}
